c9afd03802327a0b2a3eee98cbe92d3387f779e1
paseri3739 <tael43@icloud.com>
2023-07-14 14:24:10 +0900

    refactor 2d.ipynb

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # random_coords_for_gaussian
 random_coords.pyは乱数により複数の座標の組を持った入力ファイルを作成する。
+python(3) random_coords.py <number of loops>
 
 python(3) split_gaussian.py <logファイルのpath> でlogファイルをsplitedディレクトリに分割して格納する。
 
--- random_coords.py
+++ random_coords.py
@@ -9,6 +9,7 @@ if len(sys.argv) != 2:
 
 # constants
 MIN = 2.2  # MIN
+SUM = 5.8  # A+B(MIN)
 NUMBER_OF_SYSTEMS = 4
 ATOM_LABELS = ["Na"] * NUMBER_OF_SYSTEMS  # make a list that has 4 Na atoms
 HEADER = "%NProcShared=16\n%mem=12GB\n%Chk=checkpoint.chk\n#p B3LYP/6-311+g(d) force\n\nTest\n\n0 1\n"  # header
@@ -16,7 +17,6 @@ FILE_PATH = "./output/rand.com"
 NUMBER_OF_LOOPS = int(sys.argv[1])  # number of loops to generate
 
 def generate_linear_random_coords(min):  # randomly generate coordinates
-    SUM = 5.8  # A+B(MIN)
     coords = [(0.0, 0.0, 0.0)]  # initialize coords list
     for _ in range(3):  # 3 means x y z coords
         last_coord = coords[-1]
@@ -26,7 +26,7 @@ def generate_linear_random_coords(min):  # randomly generate coordinates
         new_coord = (
             last_coord[0],
             last_coord[1],
-            last_coord[2] + rand * (SUM - min) + min,
+            last_coord[2] + rand * (SUM - min) + min, #r1,2,3
         )
         coords.append(new_coord)
     return coords
