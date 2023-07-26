b75c398eace0ad11adad3c9068e39c85fb9e5428
paseri3739 <tael43@icloud.com>
2023-07-26 17:51:00 +0900

    random complete

--- GaussianWriter.py
+++ GaussianWriter.py
@@ -9,9 +9,11 @@ class GaussianWriter:
     default_header: str = "%NProcShared=16\n%mem=12GB\n%Chk=checkpoint.chk\n#p B3LYP/6-311+g(d) force\n\nTest\n\n0 1\n"
 
     # コンストラクタ。原子クラスタを受け取る。インスタンス毎に変えるべき初期値はここで定義する。
-    def __init__(self, atom_cluster: AtomClusterInterface):
+    def __init__(self, atom_cluster: AtomClusterInterface, min_val: float, max_val: float):
         self.header = self.default_header  # デフォルトヘッダを代入
         self.atom_cluster = atom_cluster
+        self.min_val = min_val
+        self.max_val = max_val
 
     def set_header(self, header: str = default_header) -> None:
         self.header = header
@@ -21,6 +23,7 @@ class GaussianWriter:
             for i in range(loops):
                 if self.header is not None:
                     file.write(self.header)
+                self.atom_cluster.placing_atoms_in_a_line(self.min_val, self.max_val)  # 座標をアルゴリズムに従って配置する
                 self._write_atom_cluster(file)
                 if i < loops - 1:
                     file.write("\n--Link1--\n")
--- main.py
+++ main.py
@@ -5,11 +5,9 @@ from GaussianWriter import GaussianWriter
 if __name__ == "__main__":
     file_path = "output.txt"
     loops = 5
-    atom = Atom(atom_name="Na")
+    min_val = 2.2
+    max_val = 5.8
 
-    # atom_cluster = FourAtomCluster([atom] * 4) このように記述すると4つのAtomはすべて同じインスタンスとして生成されるため1つの座標を変更するとすべてのAtomインスタンスの座標が変更されてしまう。
-    atom_cluster = FourAtomCluster([Atom(atom_name="Na") for _ in range(4)])  # リスト内包表記を利用する。
-    atom_cluster.placing_atoms_in_a_line(2.2, 5.8)  # 座標をアルゴリズムに従って配置する
-    atom_cluster.display_atoms()
-    writer = GaussianWriter(atom_cluster)
+    atom_cluster = FourAtomCluster([Atom(atom_name="Na") for _ in range(4)])
+    writer = GaussianWriter(atom_cluster, min_val, max_val)  # min_val, max_valを追加
     writer.write(file_path, loops)
