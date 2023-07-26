24ea881a3e21bb0a7a79bf15775fde1afc833ca3
paseri3739 <tael43@icloud.com>
2023-07-26 16:03:14 +0900

    random bugs

--- FourAtomCluster.py
+++ FourAtomCluster.py
@@ -10,37 +10,44 @@ class FourAtomCluster(AtomClusterInterface):
     def __init__(self, atoms: list[Atom]):
         if len(atoms) != 4:
             raise ValueError("FourAtomCluster must have 4 atoms.")
-        super().__init__(atoms)
+        self.atoms = atoms
 
     def placing_atoms_in_a_plane(self, min_val: float, max_val: float) -> list[np.ndarray]:
-        origin = np.array([0.0, 0.0, 0.0])
-        randoms = [1.0, 1.0, 1.0]
-        angles = [0.2, 0.2]
+        # Updated to assign different coordinates to each atom
+        points = []
+        for atom in self.atoms:
+            origin = np.array([0.0, 0.0, 0.0])
+            randoms = [1.0, 1.0, 1.0]
+            angles = [0.2, 0.2]
 
-        for i in range(3):
-            randoms_seed = random.uniform(0, 1)
-            randoms[i] = randoms_seed * (max_val - min_val) + min_val
+            random.seed()  # Seed the random number generator once
 
-        for i in range(2):
-            angle_seed = random.uniform(0, 1)
-            angles[i] = angle_seed * np.pi
+            for i in range(3):
+                randoms[i] = random.uniform(min_val, max_val)
 
-        p1 = np.array([origin[0] + randoms[0], origin[1], origin[2]])
-        p2 = np.array([p1[0] + randoms[1] * np.cos(angles[0]), p1[1] + randoms[1] * np.sin(angles[0]), origin[2]])
-        p3 = np.array(
-            [
-                p2[0] + randoms[2] * np.cos(angles[0] + angles[1]),
-                p2[1] + randoms[2] * np.sin(angles[0] + angles[1]),
-                origin[2],
-            ]
-        )
-        points = [origin, p1, p2, p3]
+            for i in range(2):
+                angles[i] = random.uniform(0, np.pi)
 
-        # Update the coordinates of the atoms with the generated points
-        for i, point in enumerate(points):
-            self.atoms[i].coordinates = point
+            p1 = np.array([origin[0] + randoms[0], origin[1], origin[2]])
+            p2 = np.array([p1[0] + randoms[1] * np.cos(angles[0]), p1[1] + randoms[1] * np.sin(angles[0]), origin[2]])
+            p3 = np.array(
+                [
+                    p2[0] + randoms[2] * np.cos(angles[0] + angles[1]),
+                    p2[1] + randoms[2] * np.sin(angles[0] + angles[1]),
+                    origin[2],
+                ]
+            )
+            points = [origin, p1, p2, p3]
+
+            # Update the coordinates of the atoms with the generated points
+            for i, point in enumerate(points):
+                self.atoms[i].coordinates = point
 
         return points
+
+    def print_points(self, points: list[np.ndarray]):
+        for point in points:
+            print(point)
 
     def placing_atoms_in_a_line(self, min_val: float, max_val: float) -> list[np.ndarray]:
         coords = np.zeros((len(self.atoms), 3))
--- main.py
+++ main.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
     atom = Atom(atom_name="Na")
 
     atom_cluster = FourAtomCluster([atom] * 4)  # 4つの原子が原点に配置された状態でインスタンスが作成される
-    atom_cluster.placing_atoms_in_a_plane(2.2, 5.8)  # 座標をアルゴリズムに従って配置する
+    # atom_cluster.placing_atoms_in_a_plane(2.2, 5.8)  # 座標をアルゴリズムに従って配置する
+    atom_cluster.print_points(atom_cluster.placing_atoms_in_a_plane(2.2, 5.8))
     atom_cluster.display_atoms()
+
     # writer = GaussianWriter(atom_cluster)
     # writer.write(file_path, loops)
