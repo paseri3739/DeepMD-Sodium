9449725f8e015566c3ba8e561be786a1ed7fae4c
paseri3739 <tael43@icloud.com>
2023-07-29 23:01:26 +0900

    remove unused parameter and use lambda

--- GaussianWriter.py
+++ GaussianWriter.py
@@ -5,37 +5,48 @@ from AtomClusterInterface import AtomClusterInterface
 class GaussianWriter:
     default_header: str = "%NProcShared=16\n%mem=12GB\n%Chk=checkpoint.chk\n#p B3LYP/6-311+g(d) force\n\nTest\n\n0 1\n"
 
-    def __init__(self, atom_cluster: AtomClusterInterface, placing_algorithm):
+    def __init__(self, atom_cluster: AtomClusterInterface):
         self.header = self.default_header
         self.atom_cluster = atom_cluster
-        self.min_val = atom_cluster.min
-        self.max_val = atom_cluster.max
-        self.placing_algorithm = placing_algorithm
 
     def set_header(self, header: str = default_header) -> None:
         self.header = header
 
-    def write(self, file_path, loops: int) -> None:
+    def write(self, file_path, loops: int, algorithm=lambda x: x.place_atoms_in_a_plane()) -> None:
+        print("write called")
+        if self.header is None:
+            print("No header has been set.")
+            return
+
+        count = 0
         with open(file_path, "w") as file:
             for i in range(loops):
-                # Execute the placing algorithm and get the resulting cluster
-                resulting_cluster = self.placing_algorithm()
-                # Check the conditions of the cluster and get the result
-                check_result = resulting_cluster.check_and_report_conditions("none")
+                print(f"placing trial {count}")
+                cluster = algorithm(self.atom_cluster)  # Use the passed algorithm
 
-                # If the conditions are met, write the cluster to the file
-                if check_result in ["crossed", "not crossed"]:
-                    if self.header is not None:
-                        file.write(self.header)
+                print(f"condition check {count}")
+                condition = cluster.check_and_report_conditions(plot_type="none")
+
+                if condition == "not crossed":
+                    file.write(self.header)
                     self._write_atom_cluster(file)
                     if i < loops - 1:
                         file.write("\n--Link1--\n")
-            file.write("\n")
+                    count += 1  # Increment count only when condition is met
+                    print(count)
+
+                # Break the loop if we have met the condition enough times
+                if count >= loops:
+                    break
+
         print(f"Gaussian input file has been written to {file_path}")
+
+
 
     def _write_atom_cluster(self, file) -> None:
         for atom in self.atom_cluster.atoms:
             coordinates = atom.get_coordinates()
+            # If there are only two coordinates provided (2D case), append 0 as the third coordinate
             if len(coordinates) == 2:
                 coordinates.append(AtomClusterInterface.origin[2]) # add z coordinate if it is missing
             file.write(f"{atom.atom_name} {coordinates[0]} {coordinates[1]} {coordinates[2]}\n")
