0befaa7d0c810c3cc5410f702f7941095338b3de
paseri3739 <tael43@icloud.com>
2023-08-06 16:31:49 +0900

    installer added

---
+++ install.bat
@@ -0,0 +1,5 @@
+@echo off
+
+python -m venv randomcoords
+call randomcoords\Scripts\activate.bat
+pip install -r requirements.txt
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 -m venv randomcoords
+source randomcoords/bin/activate
+pip install -r requirements.txt
