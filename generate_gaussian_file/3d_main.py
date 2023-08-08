68447e5364cb9fec1afa05ce8b8f678071aeac0a
paseri3739 <tael43@icloud.com>
2023-08-08 11:09:24 +0900

    remove

--- generate_gaussian_file/3d_main.py
+++ generate_gaussian_file/3d_main.py
@@ -8,11 +8,11 @@ if __name__ == "__main__":
     max_distance = 5.8
     # 4つの異なるAtomインスタンスをNaとして作成。最短距離と最長距離を指定。
     atom_cluster_3d = FourAtomCluster.from_atom_name(atom_name="Na", count=4, min=minimum_distance, max=max_distance)
-    atom_cluster_3d.place_atoms_in_a_cube()
-    atom_cluster_3d.display_atoms()
-    atom_cluster_3d.display_atom_distances()
-    atom_cluster_3d.display_distance_condition()
-    atom_cluster_3d.plot_3d(line=True)
+    # atom_cluster_3d.place_atoms_in_a_cube()
+    # atom_cluster_3d.display_atoms()
+    # atom_cluster_3d.display_atom_distances()
+    # atom_cluster_3d.display_distance_condition()
+    # atom_cluster_3d.plot_3d(line=True)
 
     writer = GaussianWriter(atom_cluster_3d)
     writer.write(file_path, loops, algorithm=atom_cluster_3d.place_atoms_in_a_cube)
