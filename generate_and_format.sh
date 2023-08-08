19f27c9cdf0b9b02fd56c286bfcb5bad1f60faeb
paseri3739 <tael43@icloud.com>
2023-08-08 12:49:05 +0900

    automation

--- generate_and_format.sh
+++ generate_and_format.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-python3 ./generate_gaussian_file/generate_gaussian_file.py
+# if $1 is none, exit 1
+if [ -z "$1" ]; then
+    echo "No loop times specified"
+    exit 1
+fi
+
+python3 ./generate_gaussian_file/generate_gaussian_file.py "$1"
+g16 <output.com >output.log
+python3 ./postprocess/split_gaussian.py output.log
+python3 ./postprocess/import_gaussian_from_dir.py splited
