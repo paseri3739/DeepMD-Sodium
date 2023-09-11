b3c192bd8937282cdd32a7af9ae4a31daf471673
paseri3739 <tael43@icloud.com>
2023-09-11 10:19:49 +0900

    automation fix

--- automation.sh
+++ automation.sh
@@ -6,9 +6,23 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-python3 ./generate_gaussian_file/generate_gaussian_file.py "$1"
+# if $2 is none, exit 1
+if [ -z "$2" ]; then
+    echo "No dimension specified"
+    exit 1
+fi
+
+# Generate the Gaussian input file based on loop times and dimension
+python3 ./generate_gaussian_file/generate_gaussian_file.py "$1" "$2"
+
+# Dynamically set the Gaussian input and output file names based on the dimension
+input_file="output_${2}.com"
+output_file="output_${2}.log"
+
 echo "Gaussian Launching..."
-g16 <output.com> output.log
+g16 <"$input_file"> "$output_file"
 echo "Gaussian Done"
-python3 ./postprocess/split_gaussian.py output.log
+
+# Execute post-processing steps
+python3 ./postprocess/split_gaussian.py "$output_file"
 python3 ./postprocess/import_gaussian_from_dir.py splited
