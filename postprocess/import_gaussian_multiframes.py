0309ee0d5bc109fe0b1b7743e2f8a4d63d374d84
paseri3739 <tael43@icloud.com>
2023-10-05 15:49:01 +0900

    change directory name

--- postprocess/import_gaussian_multiframes.py
+++ postprocess/import_gaussian_multiframes.py
@@ -3,8 +3,7 @@ import sys
 import re
 import dpdata
 
-# Check the number of arguments. This must be in the header so that the
-# help message is printed if the user provides no arguments.
+# Check the number of arguments.
 if len(sys.argv) != 2:
     print(f"Usage: {sys.argv[0]} <input_file>")
     sys.exit(1)
@@ -18,8 +17,8 @@ if not os.path.isfile(input_file):
     sys.exit(1)
 
 # Extract the file name from the input file path and create the output file name
-base_name = os.path.basename(input_file)  # Extract the file name from the path
-name_without_ext = os.path.splitext(base_name)[0]  # Remove the extension from the file name
+base_name = os.path.basename(input_file)
+name_without_ext = os.path.splitext(base_name)[0]
 
 # Extract dimension and sequence number from the file name using regex
 match = re.match(r"(\w+)_set_(\d+)", name_without_ext)
@@ -28,14 +27,18 @@ if not match:
     sys.exit(1)
 
 dimension, sequence_number = match.groups()
-output_file_name = os.path.join("data", dimension, f"set{sequence_number}")
 
-# Create the output directory if it doesn't exist
-os.makedirs(output_file_name, exist_ok=True)
+# Define output directories for raw and npy formats
+output_dir_raw = os.path.join("data", dimension, "raw", f"set_{sequence_number}")
+output_dir_npy = os.path.join("data", dimension, "npy", f"set_{sequence_number}")
+
+# Create the output directories if they don't exist
+os.makedirs(output_dir_raw, exist_ok=True)
+os.makedirs(output_dir_npy, exist_ok=True)
 
 # Load the labeled system
 multiple = dpdata.LabeledSystem(file_name=input_file, fmt="gaussian/md")
 
 print(multiple)
-multiple.to(fmt="deepmd/raw", file_name=output_file_name, set_size=200)
-multiple.to(fmt="deepmd/npy", file_name=output_file_name, set_size=200)
+multiple.to(fmt="deepmd/raw", file_name=output_dir_raw, set_size=200)
+multiple.to(fmt="deepmd/npy", file_name=output_dir_npy, set_size=200)
